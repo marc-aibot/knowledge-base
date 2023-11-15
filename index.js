ebbeb6a54ab61fa00b787a9e8fead642da4364d6
ShamailAbbas <shamailabbas61@gmail.com>
2023-11-15 23:29:54 +0500

    moved the vectore db code from api

--- index.js
+++ index.js
@@ -29,49 +29,70 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Initialize Pinecone client and vectorStore outside the request handler
+const initializePinecone = async () => {
+  const client = new PineconeClient();
+  await client.init({
+    apiKey: process.env.PINECONE_API_KEY,
+    environment: process.env.PINECONE_ENVIRONMENT,
+  });
+  const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
+
+  const loader = new DirectoryLoader("./docs", {
+    ".json": (path) => new JSONLoader(path, "/texts"),
+    ".jsonl": (path) => new JSONLinesLoader(path, "/html"),
+    ".txt": (path) => new TextLoader(path),
+    ".csv": (path) => new CSVLoader(path, "text"),
+    ".docx": (path) => new DocxLoader(path),
+    ".pdf": (path) => new PDFLoader(path),
+  });
+
+  console.log(`loading documents `);
+  const docs = await loader.load();
+  console.log(`done loading documents `);
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 75,
+  });
+
+  console.log(`splitting documents `);
+  const output = await splitter.splitDocuments(docs);
+  console.log(` done splitting documents `);
+
+  const embeddings = new OpenAIEmbeddings();
+
+  console.log(`vectorStore started`);
+  const vectorStore = await PineconeStore.fromDocuments(output, embeddings, {
+    pineconeIndex,
+  });
+  console.log(` done vectorStore start`);
+
+  return vectorStore;
+};
+
+let vectorStorePromise = initializePinecone();
+
 app.post("/api/answer", async (req, res) => {
   try {
     const memoryUsedBeforeModel = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Memory used before creating model: ${memoryUsedBeforeModel} MB`);
+    console.log(
+      `Memory used before creating model: ${memoryUsedBeforeModel} MB`
+    );
 
     const model = new OpenAI({ temperature: 0 });
 
     const memoryUsedBeforeClient = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Memory used before creating PineconeClient: ${memoryUsedBeforeClient} MB`);
+    console.log(
+      `Memory used before creating PineconeClient: ${memoryUsedBeforeClient} MB`
+    );
 
-    const client = new PineconeClient();
-    await client.init({
-      apiKey: process.env.PINECONE_API_KEY,
-      environment: process.env.PINECONE_ENVIRONMENT,
-    });
-    const pineconeIndex = client.Index(process.env.PINECONE_INDEX);
-
-    // ... (rest of your logic)
-    const loader = new DirectoryLoader("./docs", {
-      ".json": (path) => new JSONLoader(path, "/texts"),
-      ".jsonl": (path) => new JSONLinesLoader(path, "/html"),
-      ".txt": (path) => new TextLoader(path),
-      ".csv": (path) => new CSVLoader(path, "text"),
-      ".docx": (path) => new DocxLoader(path),
-      ".pdf": (path) => new PDFLoader(path),
-    });
-    const docs = await loader.load();
-
-    const splitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 75,
-    });
-
-    const output = await splitter.splitDocuments(docs);
-
-    const embeddings = new OpenAIEmbeddings();
-
-    const vectorStore = await PineconeStore.fromDocuments(output, embeddings, {
-      pineconeIndex,
-    });
+    const vectorStore = await vectorStorePromise;
 
     const question = req.body.question;
+    console.log(`starting similarity search`);
     const results = await vectorStore.similaritySearch(question);
+    console.log(`done similarity search `);
 
     const vectorStoreRetriever = vectorStore.asRetriever();
 
@@ -79,17 +100,22 @@ app.post("/api/answer", async (req, res) => {
       model,
       vectorStoreRetriever
     );
+
     const chat_history = req.body.chat_history || [];
     const answer = await chain.call({ question, chat_history });
     chat_history.push({ question, answer: answer.text });
 
     const memoryUsedBeforeAnswer = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Memory used before sending the answer: ${memoryUsedBeforeAnswer} MB`);
+    console.log(
+      `Memory used before sending the answer: ${memoryUsedBeforeAnswer} MB`
+    );
 
     res.json({ answer: answer.text, chat_history });
-    
+
     const memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`The current heap memory usage is approximately ${memoryUsed} MB.`);
+    console.log(
+      `The current heap memory usage is approximately ${memoryUsed} MB.`
+    );
   } catch (error) {
     console.log("error", error);
   }
